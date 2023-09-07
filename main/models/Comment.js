75603d532704de7e39e26b9b34b03fbbde63683a
AllenHirmiz <allenaliraqi@gmail.com>
2023-09-07 03:32:38 +0300

    update DB models

---
+++ main/models/Comment.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Activity extends Model {}
+
+Activity.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'posts',
+        key: 'id',
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'activity',
+  }
+);
+
+module.exports = Activity;
---
+++ main/models/index.js
@@ -0,0 +1,22 @@
+const User = require('./User');
+const Posts = require('./Posts');
+const Comments = require('./Comments');
+
+User.hasMany(Posts, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Posts.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Comments.belongsTo(Posts, {
+  foreignKey: 'post_id',
+});
+
+module.exports = { User, Activity };
